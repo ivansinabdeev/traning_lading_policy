a6faf4b29efa4e5f8388814b01e2d0de3572c49e
ivansinabdeev <75542138+ivansinabdeev@users.noreply.github.com>
2025-01-17 18:22:00 -0300

    base reset css / font / start header

---
+++ js/mob-menu.js
@@ -0,0 +1,36 @@
+(() => {
+  const mobileMenu = document.querySelector(".js-menu-container");
+  const openMenuBtn = document.querySelector(".js-open-menu");
+  const closeMenuBtn = document.querySelector(".js-close-menu");
+  // добавили доп класс что бы закрыть после нажатия на ссылку
+  const menuLinks = document.querySelectorAll(".mobile-modal-link");
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+    openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+    mobileMenu.classList.toggle("is-open");
+
+    const scrollLockMethod = !isMenuOpen
+      ? "disableBodyScroll"
+      : "enableBodyScroll";
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  //  добавили что бы закрывалось при нажатии на ссылку
+  menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener("click", toggleMenu);
+  });
+
+  // стандартно из скрипта от Репеты
+  openMenuBtn.addEventListener("click", toggleMenu);
+  closeMenuBtn.addEventListener("click", toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove("is-open");
+    openMenuBtn.setAttribute("aria-expanded", false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
